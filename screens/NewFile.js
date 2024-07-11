15e8672672fb7582d7d17dab9d9ccc4902fd56cb
Akshay7772208 <akshaybarve2002@gmail.com>
2024-07-11 10:06:07 +0530

    Pushing a new file

---
+++ screens/NewFile.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const NewFile = () => {
+  return (
+    <View>
+      <Text>NewFile</Text>
+    </View>
+  )
+}
+
+export default NewFile
